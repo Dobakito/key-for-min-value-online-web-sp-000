12ef64bbaeea6446b0bb653cdf97d2b66e86ca30
Michael Dobak <mdobak44@icloud.com>
2021-02-12 21:47:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  small_value == 0 
+  small_value == nil
   name_hash.each do |key, value|
     if small_value == nil || value < small_value
       small_value = value
