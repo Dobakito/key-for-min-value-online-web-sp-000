6b45dc62096546d8a65edfbc2be408d6781a29f1
Michael Dobak <mdobak44@icloud.com>
2021-02-12 21:48:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,11 @@
 
 def key_for_min_value(name_hash)
   small_value == nil
+  small_key == nil
   name_hash.each do |key, value|
     if small_value == nil || value < small_value
       small_value = value
     end 
   end
-  small_value
+  small_key
 end
